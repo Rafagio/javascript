83bcd62c42d648fdcc1b0ff60153b4fc679ba1f3
Rafael <giovanetti610@gmail.com>
2023-07-05 14:34:58 -0300

    Create Type-numer.js
    Exemplos aula01

---
+++ Type-numer.js
@@ -0,0 +1,18 @@
+v// tipo number
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 3;
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log(operacaoMatematica)
+// outros operadores / para divisão e  - subtração;
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5
+const novaOperacao = primeiroNumero/numeroPontoFlutuante;
+console.log(novaOperacao)
+
+//Nan -> Not a Number (não é um número)
+const joao = "joao";
+console.log(joao*primeiroNumero)
