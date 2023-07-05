07e3b4f31637e7c97d0257510c1f1734748cc209
Rafael <giovanetti610@gmail.com>
2023-07-05 14:35:43 -0300

    Create type-string.js
    Exemplo aula01

---
+++ type-string.js
@@ -0,0 +1,21 @@
+// criação de texto entre aspas, pode ser aspas duplas ou siomples
+
+const texto1 = "Olá, mundo";
+const texto2 = 'Olá mundo';
+const senha = "superSenha!";
+const stringDeNumeros = "345676";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo";
+
+
+
+
+//template string ou template literal;
+
+//concatenacao (+)
+console.log(citacao+meuNome)
+
+
+
+//template string ou template literal
