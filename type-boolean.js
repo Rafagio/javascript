33c78ead4bca5d420cf839611c5b3d9f7faa6035
Rafael <giovanetti610@gmail.com>
2023-07-05 14:37:21 -0300

    Type-boolean
    Exemplo aula01

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+
+//boolean verdadeiro ou falso (true, false)
+const primeiroNumero = 3;
+const segundoNumero = 4;
+
+
+//comparacao entre valores ===
+//comparacao entre senhas por exemplo
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1= "joao";
+const texto2="b"
